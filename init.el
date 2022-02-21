2de6ffe7f20e9a990a8d9820ab2c9ba2d1b9e0cf
Michael Esch <eschmichael@pm.me>
2022-02-21 18:55:23 -0500

    use default utf-8 for everything

--- init.el
+++ init.el
@@ -4,7 +4,12 @@
 (tooltip-mode -1)
 (set-fringe-mode 10)
 (menu-bar-mode -1)
-(setq visible-bell t)
+(prefer-coding-system       'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8
+      visible-bell t)
 
 ;; load slip's stuff
 (add-to-list 'load-path "~/.emacs.d/lisp/")
